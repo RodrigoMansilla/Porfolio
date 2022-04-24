42c1f5871d1bfbe2c830149e448187d84a6aa135
Rodrigo Mansilla <rodri_gnr@hotmail.com.ar>
2022-04-24 05:52:33 -0300

    [Add]Api del clima
    Se agrega como pequeño proyecto una api que muestra los datos en tiempo real del tiempo de la cuidad tomadas por su localizacion, fuente : openweathermap.org

--- index.js
+++ index.js
@@ -1,3 +1,83 @@
+
+window.addEventListener('load', ()=> {
+  let lon
+  let lat
+
+  let temperaturaValor = document.getElementById('temperatura-valor')  
+  let temperaturaDescripcion = document.getElementById('temperatura-descripcion')  
+  
+  let ubicacion = document.getElementById('ubicacion')  
+  let iconoAnimado = document.getElementById('icono-animado') 
+
+  let vientoVelocidad = document.getElementById('viento-velocidad') 
+
+
+  if(navigator.geolocation){
+     navigator.geolocation.getCurrentPosition( posicion => {
+         lon = posicion.coords.longitude
+         lat = posicion.coords.latitude
+           
+         const url = `https://api.openweathermap.org/data/2.5/weather?units=metric&lang=es&lat=${lat}&lon=${lon}&appid=3459b0a41b001caebb6d3a4910025e62`
+
+         //ubicación por ciudad
+        //const url = `https://api.openweathermap.org/data/2.5/weather?q=Madrid&lang=es&units=metric&appid=3459b0a41b001caebb6d3a4910025e62`
+         fetch(url)
+          .then( response => { return response.json()})
+          .then( data => {
+              
+              
+              let temp = Math.round(data.main.temp)
+              
+              temperaturaValor.textContent = `${temp} ° C`
+
+              //console.log(data.weather[0].description)
+              let desc = data.weather[0].description
+              temperaturaDescripcion.textContent = desc.toUpperCase()
+              ubicacion.textContent = data.name
+              
+              vientoVelocidad.textContent = `${data.wind.speed} m/s`
+              
+              //para iconos estáticos
+              //const urlIcon = `http://openweathermap.org/img/wn/${iconCode}.png`                     
+              //icono.src = urlIcon
+              //console.log(data.weather[0].icon)
+
+              //para iconos dinámicos
+              
+              switch (data.weather[0].main) {
+                  case 'Thunderstorm':
+                    iconoAnimado.src='animated/thunder.svg'
+                    break;
+                  case 'Drizzle':
+                    iconoAnimado.src='animated/rainy-2.svg'
+                    break;
+                  case 'Rain':
+                    iconoAnimado.src='animated/rainy-7.svg'
+                    break;
+                  case 'Snow':
+                    iconoAnimado.src='animated/snowy-6.svg'
+                    break;                        
+                  case 'Clear':
+                      iconoAnimado.src='animated/day.svg'
+                    break;
+                  case 'Atmosphere':
+                    iconoAnimado.src='animated/weather.svg'
+                      break;  
+                  case 'Clouds':
+                      iconoAnimado.src='animated/cloudy-day-1.svg'
+                      break;  
+                  default:
+                    iconoAnimado.src='animated/cloudy-day-1.svg'
+                }
+
+          })
+          .catch( error => {
+              console.log(error)
+          })
+     })
+        
+  }
+})
 function CopyPassword() {
   document.getElementById("alertas").innerHTML = "";
   var copyText = document.getElementById("result");
